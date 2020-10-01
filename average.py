af68267ee25a6270e015da58ac6d82fe3c80d0fa
ShaneHannigan <71726128+ShaneHannigan@users.noreply.github.com>
2020-10-01 09:55:04 -0400

    Add files via upload

--- average.py
+++ average.py
@@ -1,13 +1,12 @@
-Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> numberOfTests = int(input("Please enter number of tests you want to average : "))
-Please enter number of tests you want to average : 15
->>> score1 = float(input("Please enter a score "))
-Please enter a score 90
->>> score2 = float(input("Please enter a score: "))
-Please enter a score: 88
->>> average = (score1 + score2)/2
->>> print("The average is ", average)
-The average is  89.0
->>> 
+numberOfTests = 0
+score = 0
+total = 0
+average = 0.0
+scoreCount = 0
+
+numberOfTests = int(input("Please enter the number of tests you want to average: "))
+score = int(input("Please enter a score: "))
+scoreCount = scoreCount + 1
+total = total + score
+average = total/scorecount
+print("The average os ",average)
