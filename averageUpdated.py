e0d2214c61442e16a8f798b616e18f6f1c924e56
ShaneHannigan <71726128+ShaneHannigan@users.noreply.github.com>
2020-10-01 09:55:30 -0400

    Add files via upload

---
+++ averageUpdated.py
@@ -0,0 +1,12 @@
+numberOfTests = 0
+score = 0
+total = 0
+average = 0.0
+scoreCount = 0
+
+numberOfTests = int(input("Please enter the number of tests you want to average: "))
+score = int(input("Please enter a score: "))
+scoreCount = scoreCount + 1
+total = total + score
+average = total/scorecount
+print("The average os ",average)
